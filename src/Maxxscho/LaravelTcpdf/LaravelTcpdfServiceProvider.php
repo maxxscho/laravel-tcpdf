8a2bab11b1af8870628b0df3ad113e4276609691
Markus Schober <maxxscho@inode.at>
2013-12-10 17:39:19 +0100

    Initial Commit

---
+++ src/Maxxscho/LaravelTcpdf/LaravelTcpdfServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Maxxscho\LaravelTcpdf;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTcpdfServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('maxxscho/laravel-tcpdf');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
